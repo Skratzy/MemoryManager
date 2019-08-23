e14bbb2084fdad2e04444edc10232ee38591c45a
Fredrik Junede <ettafredrik@gmail.com>
2019-08-23 10:48:03 +0200

    Initial commit of previous project

---
+++ MemoryManager/src/Defines.h
@@ -0,0 +1,70 @@
+#ifndef _MEMMNGR_DEFINES_
+#define _MEMMNGR_DEFINES_
+
+#include <iostream>
+#include <assert.h>
+#include <malloc.h>
+
+constexpr auto ARCH_BYTESIZE = sizeof(void*);
+
+#define MEMMNGR_ASSERT(s) assert(s)
+
+/*
+	s: Size in bytes
+*/
+#define MEMMNGR_MALLOC(s) malloc(s)
+
+/*
+	s: Size in bytes
+*/
+#define MEMMNGR_MALLOC_POOL_ALLOC(s)
+constexpr auto PERSISTENT_STACK_INDEX = 0;
+constexpr auto FUNCTION_STACK_INDEX = 1;
+
+#define MEMMNGR_FREE(s) free(s)
+#define MEMMNGR_DELETE(s) delete s
+
+/*
+	s: Debug message - to be printed
+	l: Severity level
+		0 -> print the message
+		1 -> print the message and abort
+*/
+/*#define MEMMNGR_DEBUG_MESSAGE(s, l) { \
+	HANDLE stdHandle = GetStdHandle(STD_OUTPUT_HANDLE); \
+	CONSOLE_SCREEN_BUFFER_INFO conScrBufInf; \
+	GetConsoleScreenBufferInfo(stdHandle, &conScrBufInf); \
+	switch(l) { \
+	case 0: \
+		SetConsoleTextAttribute(stdHandle, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY); \
+		std::cerr << "WARNING: " << s << std::endl; \
+		SetConsoleTextAttribute(stdHandle, conScrBufInf.wAttributes); \
+		break; \
+	case 1:\
+		SetConsoleTextAttribute(stdHandle, BACKGROUND_RED | BACKGROUND_INTENSITY);\
+		std::cerr << "ERROR: " << s << std::endl;\
+		SetConsoleTextAttribute(stdHandle, conScrBufInf.wAttributes);\
+		abort();\
+		break;\
+	default: break;\
+}}*/
+
+/*
+	s: Debug message - to be printed
+	l: Severity level
+		0 -> print the message
+		1 -> print the message and abort
+*/
+#define MEMMNGR_DEBUG_MESSAGE(s, l) { \
+	switch(l) { \
+	case 0: \
+		std::cerr << "WARNING: " << s << std::endl; \
+		break; \
+	case 1:\
+		std::cerr << "ERROR: " << s << std::endl;\
+		abort();\
+		break;\
+	default: break;\
+}}
+
+#endif //_MEMMNGR_DEFINES_
---
+++ MemoryManager/src/StackAllocator.cpp
@@ -0,0 +1,97 @@
+#include "Defines.h"
+#include "StackAllocator.h"
+
+#include <string>
+
+/*+-+-+-+-+-+-+-+-+-+-+-+
+	PRIVATE FUNCTIONS
++-+-+-+-+-+-+-+-+-+-+-+*/
+
+unsigned int StackAllocator::padMemory(unsigned int sizeBytes) {
+	unsigned int paddedMemory = sizeBytes % ARCH_BYTESIZE;
+	// If padding needed, determine and add padding
+	if (paddedMemory != 0)
+		paddedMemory = sizeBytes + (ARCH_BYTESIZE - paddedMemory);
+	// Else we don't need padding
+	else
+		paddedMemory = sizeBytes;
+
+	return paddedMemory;
+}
+
+void StackAllocator::cleanUp() {
+	if (m_memPtr != nullptr) {
+		free(m_memPtr);
+		m_memPtr = nullptr;
+	}
+}
+
+
+
+
+
+/*+-+-+-+-+-+-+-+-+-+-+-+
+	 PUBLIC FUNCTIONS
++-+-+-+-+-+-+-+-+-+-+-+*/
+
+
+StackAllocator::StackAllocator(void * memPtr, unsigned int sizeBytes) : Allocator(memPtr, padMemory(sizeBytes)) {
+	m_marker.store(0);
+}
+
+StackAllocator::~StackAllocator() {
+	cleanUp();
+}
+
+// Automatically pads the allocation
+void* StackAllocator::allocate(unsigned int sizeBytes) {
+	char* ptrToStackTop = nullptr;
+	unsigned int paddedAllocation = padMemory(sizeBytes);
+
+	if (m_marker + paddedAllocation <= m_sizeBytes) {
+		// Get current marker location and move marker to top (where we're currently allocating)
+		Marker currMarker = m_marker.fetch_add(paddedAllocation);
+
+		ptrToStackTop = static_cast<char*>(m_memPtr) + currMarker;
+	}
+	else // Else if we're allocating past the max size of the stack
+		throw std::overflow_error("StackAllocator::allocate : Tried to allocate more memory than was available.");
+
+	return ptrToStackTop;
+}
+
+// Deallocates all memory
+void StackAllocator::deallocateAll() {
+	m_marker.store(0);
+}
+
+// Gets the current location of the stack marker
+Marker StackAllocator::getMarker() {
+	return m_marker;
+}
+
+// Clears to the stack to the marker
+void StackAllocator::clearToMarker(Marker marker) {
+	if (marker < m_marker)
+		m_marker.store(marker);
+	else
+		MEMMNGR_DEBUG_MESSAGE("Tried to clear to (" + std::to_string(marker) + ") when current marker was at (" + std::to_string(m_marker) + ").", 0);
+}
+
+std::vector<bool> StackAllocator::getUsedMemory() {
+	// A vector of bools that represents used memory
+	std::vector<bool> usedMemory;
+	usedMemory.resize(100);
+	// Calculate percentage of total memory used
+	unsigned int percentUsed = int(float(m_marker) / float(m_sizeBytes) * 100.f);
+	// As long as 'i' is < 'percentUsed', we continue setting 'true' (to-be-drawn)
+	for (unsigned int i = 0; i < 100; i++)
+		usedMemory.at(i) = ((i < percentUsed) ? true : false);
+
+	return usedMemory;
+}
+
+float StackAllocator::getUsedMemoryPercentage() {
+	// Percentage of used memory
+	return float(m_marker) / float(m_sizeBytes);
+}
