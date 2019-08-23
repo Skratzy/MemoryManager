f5ba5dca91eba4ce11180ca15a9ac93435d08b31
Fredrik Junede <ettafredrik@gmail.com>
2019-08-23 12:14:54 +0200

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+-- premake5.lua
+project "MemoryManager"
+	kind "StaticLib"
+	language "C++"
+	
+	targetdir (binDir)
+	objdir (intermediatesDir)
+
+	files {
+		"MemoryManager/src/Allocator.h",
+		"MemoryManager/src/MemoryManager.h",
+		"MemoryManager/src/Defines.h",
+		"MemoryManager/src/PoolAllocator.h",
+		"MemoryManager/src/StackAllocator.h",
+		"MemoryManager/src/Allocator.cpp",
+		"MemoryManager/src/MemoryManager.cpp",
+		"MemoryManager/src/PoolAllocator.cpp",
+		"MemoryManager/src/StackAllocator.cpp"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+		
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
